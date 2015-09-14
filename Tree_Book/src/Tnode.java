0ba712ab65dc21bae0ca4c560ccd4e2bbc290520
sanvsam11 <sanvsam11@gmail.com>
2015-09-14 20:10:31 +0530

    tree

---
+++ Tree_Book/src/Tnode.java
@@ -0,0 +1,11 @@
+/**
+ * Created by sant on 14/9/15.
+ */
+public class Tnode {
+    char element;
+    Tnode left,right;
+
+    public Tnode(char element){
+        this.element=element;left=right=null;
+    }
+}
