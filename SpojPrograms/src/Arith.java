532f75cf55301c9aa8373656a6df282aaf703140
sanvsam11 <sanvsam11@gmail.com>
2015-09-17 13:22:35 +0530

    I'm so dead

--- SpojPrograms/src/Arith.java
+++ SpojPrograms/src/Arith.java
@@ -2,31 +2,37 @@ public class Arith
 {
 	public static class Formula{
 		String inp;
-		char or1,or2,op;int l,s;
+		String or1,or2;char op;int l,s,a,b;
 		public Formula(String inp){this.inp=inp;}
 		public void parser(){l=inp.length();
 			for(int i=0;i<l;i++){
 				if(inp.charAt(i)=='+'||inp.charAt(i)=='-'||inp.charAt(i)=='*'||inp.charAt(i)=='/'){
-				or1=inp.charAt(i-1);op=inp.charAt(i);or1=inp.charAt(i+1); break;	
+				or1=inp.substring(0,i);op=inp.charAt(i);or2=inp.substring(i+1,l); break;	
 				}
-			}
+			}a=Integer.parseInt(or1);b=Integer.parseInt(or2);
 		}
 		public void Compute(){
 			switch(op){
-				case '+':s=or1+or2;break;
-				case '-':s=or1-or2;break;
-				case '*':s=or1*or2;break;
-				case '/':s=or1/or2;break;
+				case '+':s=a+b;break;
+				case '-':s=a-b;break;
+				case '*':s=a*b;break;
+				case '/':s=a/b;break;
 			}System.out.println("Solution:"+s);
 		}
-	}
+	
+	public void Display(){
+		System.out.println(or1+op+or2+"="+s);
+		System.out.println(a+" "+op+" "+b+"="+s);
+	}}
 	public static void main (String ar[]){
 		
 		String inp=new String();
-		inp="24+26";
+		inp="24*37";
 		Formula qn=new Formula(inp);
 		qn.parser();
+		
 		qn.Compute();
+		//qn.Display();
 		
 	}
 }
