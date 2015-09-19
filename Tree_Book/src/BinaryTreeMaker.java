fb75b09d77fe9fc017de52a7ca1a69077a4ae3ce
sanvsam11 <sanvsam11@gmail.com>
2015-09-19 12:01:53 +0530

    bt bug

--- Tree_Book/src/BinaryTreeMaker.java
+++ Tree_Book/src/BinaryTreeMaker.java
@@ -1,18 +1,16 @@
 /**
  * Created by sant on 14/9/15.
- */
-public class BinaryTreeMaker{
-    QueueMaker BTEnabler=new QueueMaker();
-    QueueMaker TPrintQueue=new QueueMaker();
-    tnode Root,c;
+ 
 
     public BinaryTreeMaker(){Root=null;}
-    public void InsertNode(char element){ c=Root;
+    public void InsertNode(char element){
         while(true){
-            if(c==null){tnode newnode=new tnode(element);Root=newnode;BTEnabler.push(newnode);TPrintQueue.push(newnode);break;}
-            else{c=BTEnabler.pop();
+            if(Root==null){tnode newnode=new tnode(element);c=
+                    Root=newnode;BTEnabler.push(newnode);TPrintQueue.push(newnode);break;}
+            else{
                 if(c.left==null){tnode newnode=new tnode(element);c.left=newnode;BTEnabler.push(newnode);break;}
-                else {tnode newnode=new tnode(element);c.right=newnode;BTEnabler.push(newnode);break;}
+                else if(c.right==null) {tnode newnode=new tnode(element);c.right=newnode;BTEnabler.push(newnode);break;}
+                else c=BTEnabler.pop();
             }
         }
     }
