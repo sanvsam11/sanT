285820e67076afe6fa7fdcf3ea058183cbd92d6b
sanvsam11 <sanvsam11@gmail.com>
2015-09-19 16:13:43 +0530

    added stackmaker

--- Tree_Book/src/StackMaker.java
+++ Tree_Book/src/StackMaker.java
@@ -2,7 +2,7 @@
  * Created by sant on 19/9/15.
  */
 public class StackMaker {
-    qnode top;
+    qnode top,c;
 
     public StackMaker(){
         top=null;
@@ -16,5 +16,25 @@ public class StackMaker {
     public tnode pop(){ tnode tmp;
         if(top==null)return null;
         else {tmp=top.element;top=top.next;return tmp;}
+    }
+    public void PrintStack(){c=top;
+        while (c!=null){System.out.println(c.element.element);c=c.next;}
+    }
+    public static void main(String ar[]){
+      /*
+        tnode a=new tnode('a');
+        tnode b=new tnode('b');
+        tnode c=new tnode('c');
+        tnode d=new tnode('d');
+
+        StackMaker teststack=new StackMaker();
+        teststack.push(a);
+        teststack.push(b);
+        teststack.push(c);
+        teststack.push(d);
+        teststack.PrintStack();
+        System.out.println("pop:"+teststack.pop().element);
+        teststack.PrintStack();
+        */
     }
 }
