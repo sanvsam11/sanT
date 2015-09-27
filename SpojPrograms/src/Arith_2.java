b2d9bde4e798c9e4275b65651ccbdf1b5ba59e22
sanvsam11 <sanvsam11@gmail.com>
2015-09-27 09:34:14 +0530

    arith in progress

--- SpojPrograms/src/Arith_2.java
+++ SpojPrograms/src/Arith_2.java
@@ -1,6 +1,14 @@
 public class Arith_2
 {
-	BinaryTreeMaker expTree=new BinaryTreeMaker();
+	public static void main(String ar[])
+	{
+		CQueueMaker question=new CQueueMaker();
+		String exp="+*ab/cd";int i=0;
+		while(i<exp.length()){
+			question.push(exp.charAt(i));
+		i++;}
+		ExpressionTreeBuilder soltree=new ExpressionTreeBuilder(question);
 
-    
+	}
+
 }
---
+++ SpojPrograms/src/ExpressionTreeBuilder.java
@@ -0,0 +1,32 @@
+/**
+ * Created by sant on 27/9/15.
+ */
+public class ExpressionTreeBuilder {
+    CQueueMaker input=new CQueueMaker();
+
+    tnode Root;
+    public ExpressionTreeBuilder(CQueueMaker input){
+        this.input=input;
+    }
+
+    public tnode TreeBuilder(tnode c){if(!input.IsEmpty()) {
+        char ch = input.pop();
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            if (Root == null) {
+                Root = new tnode(ch);
+                c = Root;
+            } else {
+                c = new tnode(ch);
+            }
+            c.left = TreeBuilder(c.left);
+            c.right = TreeBuilder(c.right);
+        } else {
+            c = new tnode(ch);
+        }
+    }
+    return c;}
+
+    public int ExpressionTreeEvaluator(int [] input){int answer=0;
+        if()
+    return answer;}
+}
