e0e7304c4f5aa2ea564326e4b0bbe7d62391896c
sanvsam11 <sanvsam11@gmail.com>
2015-10-04 10:26:06 +0530

    fold works

--- SpojPrograms/src/Linked_List_Fold.java
+++ SpojPrograms/src/Linked_List_Fold.java
@@ -2,14 +2,15 @@
 import java.util.*;
 public class Linked_List_Fold
 {
-	node r,f;
+	node r,f;static boolean end=false;
 	LinkedListMaker list=new LinkedListMaker();
-	public void ListGetter(LinkedListMaker list){this.list=list;r=f=list.head;}
-	public void Folder(node r){
-		if(r.next!=null)Folder(r.next);
+	public void ListGetter(LinkedListMaker list){this.list=list;r=f=list.head;end=false;}
+	public void Folder(node rt){
+		if(rt.next!=null)Folder(rt.next);
 
-		if(r!=f){
-			node t=f.next;f.next=r;r.next=t;
+		if(f.next==rt){end=true;rt.next=null;}
+		if(end==false){
+			node t=f.next;f.next=rt;rt.next=t;f=rt.next;
 		}
 	}
 	public static void main(String ar[]){
@@ -20,7 +21,10 @@ public class Linked_List_Fold
 		ob.list.InsertNode('d');
 		ob.list.InsertNode('e');
 		ob.list.InsertNode('f');
-		ob.Folder(ob.list.head);
+		ob.list.InsertNode('g');
+		ob.list.InsertNode('h');
+		ob.ListGetter(ob.list);
+		ob.Folder(ob.r);
 		ob.list.PrintList();
 	}
 }
