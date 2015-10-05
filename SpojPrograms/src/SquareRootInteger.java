80e000a1280f9574a5143d9d2bf4cc95473bdef3
sanvsam11 <sanvsam11@gmail.com>
2015-10-05 13:59:25 +0530

    sqrt #2

--- SpojPrograms/src/SquareRootInteger.java
+++ SpojPrograms/src/SquareRootInteger.java
@@ -3,15 +3,16 @@ public class SquareRootInteger
 	int a,i=1;
 	public SquareRootInteger(int a){this.a=a;}
 	public SquareRootInteger(){a=0;}
-	public void FindSquareRoot(){
-		while((i*i)<a)i++;if(i*i!=a)i--;
-		}
+	public void FindSquareRoot(){while((i*i)<a)i++;if(i*i!=a)i--;}
+	public void FindSquareRoot2(){i=a/2;//does not work
+		while(i*i>a)i=i/2;
+	}
 	public void PrintSquareRoot(){System.out.println(i);}
 
 	public static void main(String ar[]){
 		SquareRootInteger ob=new SquareRootInteger();
-		ob.a=626;
-		ob.FindSquareRoot();
+		ob.a=625;
+		ob.FindSquareRoot2();
 		ob.PrintSquareRoot();
 	}
 }
