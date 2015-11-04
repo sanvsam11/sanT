1d4cfd4ca726957d8d251026f256c6a98ac81b27
Sanvsam11 <Sanvsam11@gmail.com>
2015-11-04 19:16:47 +0530

    exp

--- GeeksForGeeks/src/ExponentModulation.java
+++ GeeksForGeeks/src/ExponentModulation.java
@@ -1,6 +1,12 @@
-public class ExponentModulation
+
+import java.util.*;public class ExponentModulation
 {
 	public static void main(String ar[]){
-		System.out.println("let the games begin");
-	}
+		//System.out.println("let the games begin");
+	
+		Scanner in = new Scanner(System.in);
+		int b = in.nextInt(),p=in.nextInt(),m=in.nextInt(),a=1;
+		for(int i=0;i<p;i++)a=a*b;
+		//System.out.println(b+" "+p+" "+m+" "+a);
+		System.out.println(a/m);}
 }
