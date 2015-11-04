f68b3cd4eb9b74f9a48897848d5793e1b342287a
sanvsam11 <sanvsam11@gmail.com>
2015-11-04 19:25:12 +0530

    g2g

--- GeeksForGeeks/src/ExponentModulation.java
+++ GeeksForGeeks/src/ExponentModulation.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
 
-import java.util.*;public class ExponentModulation
-{
-	public static void main(String ar[]){
-		//System.out.println("let the games begin");
-	
-		Scanner in = new Scanner(System.in);
-		int b = in.nextInt(),p=in.nextInt(),m=in.nextInt(),a=1;
-		for(int i=0;i<p;i++)a=a*b;
-		//System.out.println(b+" "+p+" "+m+" "+a);
-		System.out.println(a/m);}
+public class ExponentModulation {
+    public static void main(String ar[]) {
+        //System.out.println("let the games begin");
+
+        Scanner in = new Scanner(System.in);
+        int b = in.nextInt(), p = in.nextInt(), m = in.nextInt(), a = 1;
+        for (int i = 0; i < p; i++) a = a * b;
+        //System.out.println(b+" "+p+" "+m+" "+a);
+        System.out.println(a / m);
+    }
 }
