7da648646d6e644fb61c944c260a04005e714980
sanvsam11 <sanvsam11@gmail.com>
2015-12-04 15:47:27 +0530

    finally completed recsort

--- GeeksForGeeks/src/recursiveSortofStack.java
+++ GeeksForGeeks/src/recursiveSortofStack.java
@@ -13,18 +13,10 @@ public class recursiveSortofStack
 
 //=======
 	public void recursiveSort(intStack s,boolean thoor,int x){
-		if(!thoor){
-		 if(!s.isEmpty()){x=s.pop(); recursiveSort(s,false,x);}
-		 else recursiveSort(s,true,x);
-//>>>>>>> c3b49cf8582021865df60648a0a7d88647179781
-          }
-//>>>>>>> ce8154347d073c83a3b7a04030a5345d3a5a4235
-		else{
+		if(!thoor){if(!s.isEmpty()){x=s.pop();if(!s.isEmpty()) recursiveSort(s,false,x);}}
+
 			if(s.isEmpty()||x>s.top())s.push(x);
-			else {
-				int y=s.pop();recursiveSort(s,true,x);s.push(y);
-			}
-		}
+			else {int y=s.pop();recursiveSort(s,true,x);s.push(y);}
 	}
 		
 		
@@ -35,6 +27,9 @@ public class recursiveSortofStack
 		intStack s=new intStack(3);
 		s.push(2);
 		s.push(1);
+		s.push(4);
+		s.push(0);
+		s.push(5);
 		ob.recursiveSort(s,false,0);
 		s.printStack();
 	}
--- GeeksForGeeks/src/squaresinrect.java
+++ GeeksForGeeks/src/squaresinrect.java
@@ -2,11 +2,11 @@
 import java.util.*;public class squaresinrect
 {
 	public int squarecount(){
-		
+		return 0;
 	}
 	
 	public static void main(){
 		int l,b;
-		Scanner in = new Scanner()
+		//Scanner in = new Scanner()
 	}
 }
