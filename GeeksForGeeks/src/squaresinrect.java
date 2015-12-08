f67895b7714660df83b31982dadd6cb9758e0d74
sanvsam11 <sanvsam11@gmail.com>
2015-12-08 09:14:53 +0530

    before range sol

--- GeeksForGeeks/src/squaresinrect.java
+++ GeeksForGeeks/src/squaresinrect.java
@@ -1,12 +1,23 @@
 
-import java.util.*;public class squaresinrect
+import java.util.*;
+public class squaresinrect
 {
-	public int squarecount(){
-		return 0;
+	public squaresinrect(){}
+	public int squarecount(int l, int b){
+		int squares=0,i=2,x=0;
+		if(l==b) squares = squares + 1;
+		squares = squares + l*b;//1x1
+		while(true){x=b/i;
+			if(i<=b) squares = squares + x*x;else break;
+		i++;}
+		return squares;
 	}
 	
-	public static void main(){
-		int l,b;
-		//Scanner in = new Scanner()
+	public static void main(String ar[]){
+		int l=4,b=3;
+		//Scanner in = new Scanner(System.in);
+		//l=in.nextInt();b=in.nextInt();
+		squaresinrect ob = new squaresinrect();
+		System.out.println(ob.squarecount(l,b));
 	}
 }
