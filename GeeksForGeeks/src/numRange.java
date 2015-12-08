88abeaf453ce5a8977096cd34236376a4c229c74
Sanvsam11 <Sanvsam11@gmail.com>
2015-12-08 08:39:57 +0530

    num range done

--- GeeksForGeeks/src/numRange.java
+++ GeeksForGeeks/src/numRange.java
@@ -1,10 +1,10 @@
 public class numRange
 {
 	public boolean isPresent(int x,int low,int high){
-		if((x-low)<(high))return false;else return true;
+		if(((x-low)*(high-x))<0)return false;else return true;
 	}
 	public static void main(String ar[]){
 		numRange ob=new numRange();
-		System.out.println(ob.isPresent(10,10,100));
+		System.out.println(ob.isPresent(9,10,100));
 	}
 }
