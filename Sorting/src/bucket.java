8affe331d32c571e83ac5c8be89ffd7cb63befc4
Sanvsam11 <Sanvsam11@gmail.com>
2015-12-16 19:12:37 +0530

    bugittu

---
+++ Sorting/src/bucket.java
@@ -0,0 +1,17 @@
+
+import java.util.*;public class bucket
+{
+	boolean a[];
+	public void sortfunction(int N[],int n,int m){boolean a[]=new boolean[m];
+		for(int i=0;i<n;i++){
+			
+		}
+	}
+	public static void main(String ar[]){
+		bucket ob = new bucket();
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt(),N[]=new int[n],m=0;
+		for(int i=0;i<n;i++) {N[i]=in.nextInt();if(N[i]>m)m=N[i];}
+		ob.sortfunction(N,n,m);
+	}
+}
