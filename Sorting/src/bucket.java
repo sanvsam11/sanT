9e597e92c74b177e315437e6eae4968c0edc70bf
Sanvsam11 <Sanvsam11@gmail.com>
2015-12-17 11:23:12 +0530

    bucket done

--- Sorting/src/bucket.java
+++ Sorting/src/bucket.java
@@ -1,19 +1,21 @@
 
 import java.util.*;public class bucket
 {
-	boolean a[];
-	public void sortfunction(int N[],int n,int m){boolean a[]=new boolean[m];
-		for(int i=0;i<n;i++)a[N[i]]=true;
+	public boolean a[];
+	public boolean[] sortfunction(int N[],int n,int m){boolean a[]=new boolean[m];
+		for(int i=0;i<n;i++)
+		a[N[i]-1]=true;
+		return a;
 	}
-	public void printSorted(int m){
-		for(int i=0;i<m;i++)if(a[i])System.out.println(i);
+	public void printSorted(boolean a[],int m){
+	    for(int i=0;i<m;i++)if(a[i])System.out.println(i+1);
 	}
 	public static void main(String ar[]){
 		bucket ob = new bucket();
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt(),N[]=new int[n],m=0;
 		for(int i=0;i<n;i++) {N[i]=in.nextInt();if(N[i]>m)m=N[i];}
-		ob.sortfunction(N,n,m);
-		ob.printSorted(m);
+		
+			ob.printSorted(ob.sortfunction(N,n,m),m);
 	}
 }
