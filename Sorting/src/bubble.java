e38e6a47f36e725d134a9e38126c9f91dca60fdb
sanvsam11 <sanvsam11@gmail.com>
2015-12-19 19:09:50 +0530

    bubble done

--- Sorting/src/bubble.java
+++ Sorting/src/bubble.java
@@ -1,6 +1,24 @@
+import java.util.*;
 public class bubble
 {
+	int i=0;
+	public int[] bubbleSortFunction(int a[],int n){
+		for(int i=0;i<n;i++)
+			for(int j=i+1;j<n;j++)
+			{int x;
+				if(a[i]>a[j]){
+					x=a[i];a[i]=a[j];a[j]=x;
+				}
+			}
+	return a;}
+	public void printSequence(int a[],int n){
+		for(int i=0;i<n;i++)System.out.println(a[i]);
+	}
 	public static void main(String ar[]){
-		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt(),a[]=new int[n];
+		for(int i=0;i<n;i++)a[i]=in.nextInt();
+		bubble ob = new bubble();
+		ob.printSequence(ob.bubbleSortFunction(a,n),n);
 	}
 }
