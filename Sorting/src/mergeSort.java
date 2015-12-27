f1c60103ba0581631371c508365a15acaa271579
sanvsam11 <sanvsam11@gmail.com>
2015-12-27 09:57:34 +0530

    before clsr

--- Sorting/src/mergeSort.java
+++ Sorting/src/mergeSort.java
@@ -4,9 +4,17 @@
 import java.util.*;
 public class mergeSort {
 
-    public int[] mergeSortFunction(int[] a,int n){
-        if(n%2==0&&n>2){
-
+    public int[] mergeSortFunction(int[] a,int n,int l,int r){
+        if(n==2&&a[l]>a[r]){int x=0;
+            x=a[l];a[l]=a[r];a[r]=x;
+        }
+        else if(n%2==0){
+            a=mergeSortFunction(a,n/2,0,(n/2)-1);
+            a=mergeSortFunction(a,n/2,n/2,n-1);
+        }
+        else {
+            a=mergeSortFunction(a,n/2,0,(n/2)-1);
+            a=mergeSortFunction(a,n/2,(n/2+1),n-1);
         }
         return a;}
     public void printlist(int[] a,int n){
@@ -18,6 +26,6 @@ public class mergeSort {
         System.out.println("Enter the number of elements to be sorted");
         int n = in.nextInt(),a[]=new int[n];
         for(int i=0;i<n;i++)a[i]=in.nextInt();
-        ob.printlist(ob.mergeSortFunction(a,n),n);
+        ob.printlist(ob.mergeSortFunction(a,n,0,n-1),n);
     }
 }
