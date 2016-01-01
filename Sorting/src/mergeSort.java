7ab44ff890726941c5c12325917612781d8c66e7
sanvsam11 <sanvsam11@gmail.com>
2016-01-01 21:12:23 +0530

    merge try

--- Sorting/src/mergeSort.java
+++ Sorting/src/mergeSort.java
@@ -4,17 +4,22 @@
 import java.util.*;
 public class mergeSort {
 
-    public int[] mergeSortFunction(int[] a,int n,int l,int r){
-        if(n==2&&a[l]>a[r]){int x=0;
-            x=a[l];a[l]=a[r];a[r]=x;
-        }
-        else if(n%2==0){
-            a=mergeSortFunction(a,n/2,0,(n/2)-1);
-            a=mergeSortFunction(a,n/2,n/2,n-1);
+    public int[] mergeSortFunction(int[] a,int l,int r){
+        int x=0,d=l-r,m=(l+r)/2;
+        if(d==1&&a[l]>a[r]){x=a[l];a[l]=a[r];a[r]=x;}
+        else if(d==2&&(a[l]>a[l+1]||a[l+1]>a[r])){
+            for(int i=l;i<r;i++)
+                for(int j=i+1;j<r;j++)
+                    if(a[i]>a[j]){x=a[i];a[i]=a[j];a[j]=x;}
         }
         else {
-            a=mergeSortFunction(a,n/2,0,(n/2)-1);
-            a=mergeSortFunction(a,n/2,(n/2+1),n-1);
+            a=mergeSortFunction(a,l,m);
+            a=mergeSortFunction(a,m+1,r);
+            if(a[m]>a[m+1])
+                for(int i=m+1;i<=r;i++)
+                    for(int j=m;j>=l;j--){
+                        x=a[i];a[i]=a[j];a[j]=x;
+                    }
         }
         return a;}
     public void printlist(int[] a,int n){
@@ -26,6 +31,6 @@ public class mergeSort {
         System.out.println("Enter the number of elements to be sorted");
         int n = in.nextInt(),a[]=new int[n];
         for(int i=0;i<n;i++)a[i]=in.nextInt();
-        ob.printlist(ob.mergeSortFunction(a,n,0,n-1),n);
+        ob.printlist(ob.mergeSortFunction(a,0,n-1),n);
     }
 }
