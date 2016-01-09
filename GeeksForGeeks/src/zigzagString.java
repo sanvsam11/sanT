bdd0c06c0485d368f31b6eadc05ed58109aa76cb
sanvsam11 <sanvsam11@gmail.com>
2016-01-09 07:24:13 +0530

    zigzag string

--- GeeksForGeeks/src/zigzagString.java
+++ GeeksForGeeks/src/zigzagString.java
@@ -3,9 +3,11 @@ public class zigzagString
 {
 	public static void main(String ar[]){
 		Scanner in = new Scanner(System.in);
-		String s=in.next();
-		char[] out=new char[s.length()];
-		for(int i=0;i<s.length();i++)
-			out[i]=(i%2==0)?s.charAt(),' ';
+		String s=in.next();int l=s.length(),k=0;
+		char[] out=new char[l];
+		for(int i=0;i<l;i++) if(i%2==0) {out[k]=s.charAt(i);k++;}
+		for(int i=1;i<l;i++) if(i%2!=0) {out[k]=s.charAt(i);k++;}
+
+		System.out.println(out);
 	}
 }
