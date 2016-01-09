a317c1c9d040035746ebc8e1f1b5ce7ec26a9109
Sanvsam11 <Sanvsam11@gmail.com>
2016-01-09 06:54:04 +0530

    zigzag string

---
+++ GeeksForGeeks/src/zigzagString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class zigzagString
+{
+	public static void main(String ar[]){
+		Scanner in = new Scanner(System.in);
+		String s=in.next();
+		char[] out=new char[s.length()];
+		for(int i=0;i<s.length();i++)
+			out[i]=(i%2==0)?s.charAt(),' ';
+	}
+}
