6812441132e033f8c305dd64de81cf6a98f27675
Sanvsam11 <Sanvsam11@gmail.com>
2016-01-14 07:21:48 +0530

    graph inch

--- HackerRank/src/GraphBFS.java
+++ HackerRank/src/GraphBFS.java
@@ -1,11 +1,16 @@
 import java.util.*;
+class vertex{
+	int element;
+	vertex next;
+	public vertex(int element){this.element=element;next=null;}
+}
 public class GraphBFS
 {
 	int v,e;
 	public void getVerticesEdges(int v,int e){
 		this.v=v;this.e=e;
 	}
-	public void CreateGraph(){
+	public void CreateGraph(int [][] edges){
 		
 	}
 	public static void main(String ar[]){
@@ -15,7 +20,9 @@ public class GraphBFS
 		
 			for(int i=0;i<T;i++){int v=in.nextInt(),e=in.nextInt();
 			ob[i].getVerticesEdges(v,e);
-			for(int j=0;j<e;j++){}
-			ob[i].CreateGraph();}
+			int [][] edges=new int[v][2];
+			for(int j=0;j<e;j++){edges[j][0]=in.nextInt();edges[j][1]=in.nextInt();}
+			ob[i].CreateGraph(edges);
+			}
 	}
 }
