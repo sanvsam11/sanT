f4951e1464aacee3614af9fd0c23a690f05a6922
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-08-21 09:30:20 +0530

    wildcard matching works

--- GeeksForGeeks/src/wildcard_matching.java
+++ GeeksForGeeks/src/wildcard_matching.java
@@ -8,19 +8,19 @@ public class wildcard_matching {
         pl=pat.length();tl=txt.length();
     }
     public void check(){
-        for(int i=0,j=0;j<pl;j++){
+        for(int i=0,j=0;j<pl;j++){char cp = pat.charAt(j), ct = txt.charAt(i);
             if(pat.charAt(j)=='*'){if(j==(pl-1)) break;
                 while(pat.charAt(j)=='*')j++;
                 while(pat.charAt(j)!=txt.charAt(i))i++;
             }
-            if(i==(tl-1)&&j!=(pl-1)) i=i-(pl-j-1);
-            else if(j==(pl-1)&&i==(tl-1)) {match=false;break;}
+            if((i==(tl-1))&&(j!=(pl-1))) i=i-(pl-j-1);
+            else if(j==(pl-1)&&i!=(tl-1)) {match=false;break;}
             else if(pat.charAt(j)!=txt.charAt(i)){match=false;break;}
             else i++;
         }
     }
     public static void main(String ar[]){
-        String txt="Santhosh",pat="*ant*s";
+        String txt="Santhosh",pat="*ant*sh";
         wildcard_matching wm = new wildcard_matching(pat,txt);
         wm.check();
         System.out.println("Result:"+wm.match);
