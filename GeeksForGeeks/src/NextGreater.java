e77b6b68cef2c43fc12e63d77b1796215f8ed9d1
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-09-30 23:22:14 +0530

    zoho simple

---
+++ GeeksForGeeks/src/NextGreater.java
@@ -0,0 +1,33 @@
+/**
+ * Created by SanthoshVarathan on 30/09/16.
+ */
+import java.util.*;
+public class NextGreater {
+
+    public void sortFunction(int a[],int n){
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++){
+                if(a[i]>a[j]){
+                    int x=a[i];
+                    a[i]=a[j];
+                    a[j]=x;
+                }
+            }
+    }
+    public void nextGreaterElement(int a[],int n){
+         int as[]=new int[n];
+        for(int i=0;i<n;i++)as[i]=a[i];
+        sortFunction(as,n);
+        System.out.print("{");
+        for(int i=0;i<n;i++){int nextGreat=0;
+            for(int j=0;j<n-1;j++){if(a[i]==as[j]) nextGreat=as[j+1];}
+        System.out.print(a[i]+" > "+nextGreat+",");
+        }
+        System.out.print("}");
+    }
+    public static void main(String ar[]){
+    NextGreater n = new NextGreater();
+        int a[]={2,3,7,-1,8,5,11},l=7;
+        n.nextGreaterElement(a,l);
+    }
+}
