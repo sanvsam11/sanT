554a9e56b81a5f76f8913b48d635c40671966c94
sanvsam11 <sanvsam11@gmail.com>
2016-10-14 19:41:18 +0530

    Maxsub progress

--- Java_Practice/src/Arrays/MaxSub.java
+++ Java_Practice/src/Arrays/MaxSub.java
@@ -6,16 +6,17 @@ package Arrays;
  */
 public class MaxSub {
 	public int maxSubarraySum(int a[],int n){
+int sum[]=new int[n],curmax=0,currSum=sum[0]=a[0];
+for(int i=0;i<n;i++){
+	 currSum+=a[i];
+	 if(currSum>curmax)curmax=currSum;
+}
 
-
-
-	//	<<<<<<< HEAD
-	return 0;
-//=======
-		//return 0;
-//>>>>>>> f2822634eb6869959c0e95e0f9a407f5f221be7a
+	return curmax;
 	}
     public static void main(String ar[]){
-
+int a[]={1,1,-1,1,1,-1,-1,-1};
+MaxSub m = new MaxSub();
+System.out.println(m.maxSubarraySum(a,a.length));
     }
 }
