b40998b4ae4766dfbb3414ba0fe37d34621498d4
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-10-17 08:03:11 +0530

    threading interleaving done

---
+++ Java_Practice/src/Threads/AnonymousInner.java
@@ -0,0 +1,25 @@
+package Threads;
+
+import com.sun.tools.internal.jxc.SchemaGenerator;
+
+/**
+ * Created by SanthoshVarathan on 17/10/16.
+ */
+
+public class AnonymousInner {
+    public static void main(String ar[]){
+        Thread t1 = new Thread(new Runnable(){
+            public void run(){
+                for(int i=0;i<10;i++) System.out.println("Hello:"+i);
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+            for(int i=20;i<30;i++)System.out.println("Hello:"+i);
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
---
+++ Java_Practice/src/Threads/extendClass.java
@@ -0,0 +1,20 @@
+package Threads;
+
+/**
+ * Created by SanthoshVarathan on 17/10/16.
+ */
+class extendedThread extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<10;i++)System.out.println("hello:"+i);
+    }
+}
+public class extendClass{
+    public static void main(String ar[]){
+        extendedThread t1 = new extendedThread();
+        extendedThread t2 = new extendedThread();
+
+        t1.start();
+        t2.start();
+    }
+}
