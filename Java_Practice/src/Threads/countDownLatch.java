aa5bb6529ccb37670919c3ab92f514f29cac78c2
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-10-19 08:44:35 +0530

    threading prod cons

---
+++ Java_Practice/src/Threads/countDownLatch.java
@@ -0,0 +1,39 @@
+package Threads;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by SanthoshVarathan on 19/10/16.
+ */
+class processor2 implements Runnable{
+    CountDownLatch latch;
+    public processor2(CountDownLatch latch){
+        this.latch=latch;
+    }
+    public void run(){
+        System.out.println("Start of thread");
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        latch.countDown();
+
+    }
+}
+public class countDownLatch {
+    public static void main(String ar[]){
+        CountDownLatch latch = new CountDownLatch(3);
+        ExecutorService e = Executors.newFixedThreadPool(3);
+        for(int i=0;i<3;i++) e.submit(new processor2(latch));
+        try {
+            latch.await();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        System.out.println("completed");
+    }
+}
