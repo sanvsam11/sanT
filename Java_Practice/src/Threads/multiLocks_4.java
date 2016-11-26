7f02bb846f44b46d5ec17bac9d021b14cd46415b
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2016-11-26 21:16:01 +0530

    added comments

--- Java_Practice/src/Threads/multiLocks_4.java
+++ Java_Practice/src/Threads/multiLocks_4.java
@@ -12,19 +12,19 @@ import java.util.concurrent.locks.ReentrantLock;
  * Created by SanthoshVarathan on 26/11/16.
  */
 public class multiLocks_4 implements Runnable{
-    private Random r = new Random();
+    private Random r = new Random();            //to simulate data reception or generation or calculation
     List<Integer> l1 = new ArrayList<>();
     List<Integer> l2 = new ArrayList<>();
     Object lock1 = new Object();
     Object lock2 = new Object();
     public void run(){
-        process();
+        process();             //current thread calling process that calls two methods in sequence which are synchronised seperately for access to two lists
     }
     public static void main(String ar[]){
         multiLocks_4 m = new multiLocks_4();
         Thread t1 = new Thread(m);
         Thread t2 = new Thread(m);
-        long st = System.currentTimeMillis();
+        long st = System.currentTimeMillis();               //starting time
         t1.start();
         t2.start();
         try{
@@ -33,13 +33,13 @@ public class multiLocks_4 implements Runnable{
             e.printStackTrace();
         }
 
-        long et = System.currentTimeMillis();
+        long et = System.currentTimeMillis();               //ending time
         System.out.println("Runtime: "+(et-st));
         System.out.println("l1: "+m.l1.size()+" l2: "+m.l2.size());
     }
-    public void stageOne(){
+    public void stageOne(){                     //method updating list 1
         //lock1.lock();
-        synchronized (lock1) {
+        synchronized (lock1) {                  //lock1 to restrict access to list 1
             try {
                 Thread.sleep(1);
                 l1.add(r.nextInt());
@@ -51,8 +51,8 @@ public class multiLocks_4 implements Runnable{
             //}
         }
     }
-    public void stageTwo() {
-        synchronized (lock2) {
+    public void stageTwo() {                    //method updating list 2
+        synchronized (lock2) {                  //lock2 to restrict access to list 2
             try {
                 Thread.sleep(1);
                 l2.add(r.nextInt());
@@ -65,7 +65,7 @@ public class multiLocks_4 implements Runnable{
             }
         }
 
-    public void process(){
+    public void process(){                      //method calling two mehods that access list 1 and list 2
 
         for(int i=0;i<1000;i++) {
             stageOne();
--- Java_Practice/src/Threads/pools_5.java
+++ Java_Practice/src/Threads/pools_5.java
@@ -14,14 +14,14 @@ public class pools_5 implements Runnable{
     public void run(){
         System.out.println(id+": Start");
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5000);             //simulating doing useful work
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println(id+": End");
     }
     public static void main(String ar[]){
-        ExecutorService exT = Executors.newFixedThreadPool(3);
+        ExecutorService exT = Executors.newFixedThreadPool(3); //basically number of concurrent jobs running
 
         for(int i=0;i<5;i++){
             exT.submit(new pools_5(i));
@@ -29,7 +29,7 @@ public class pools_5 implements Runnable{
         exT.shutdown();
         System.out.println("All threads submitted.");
         try {
-            exT.awaitTermination(11, TimeUnit.SECONDS);
+            exT.awaitTermination(11, TimeUnit.SECONDS);         // join for pooled threads
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
