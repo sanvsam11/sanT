1ee08ac7d87ee78be5e121628eaf9a57ab525f70
sanvsam11 <sanvsam11@gmail.com>
2016-11-28 17:57:06 +0530

    Wait notify

--- Java_Practice/src/Threads/pcWaitNotify_8.java
+++ Java_Practice/src/Threads/pcWaitNotify_8.java
@@ -10,29 +10,32 @@ import java.util.concurrent.ThreadLocalRandom;
 public class pcWaitNotify_8 {
     static List<Integer> l = new ArrayList<>(10);
     static ThreadLocalRandom tlr = ThreadLocalRandom.current();
-    public static void producer() throws InterruptedException{
-        while(true){
-            if(!l.isEmpty())l.wait();
+    public void producer() throws InterruptedException{
+       synchronized(l){
+		while(true){
+            if(!l.isEmpty())wait();
             l.add(tlr.nextInt(0,9));
             l.notify();
         }
+		}
     }
-    public static void consumer() throws InterruptedException{
-
+    public void consumer() throws InterruptedException{
+synchronized(l){
         while(true){
-            if(l.size()<8) l.wait();
+            if(l.size()<8) wait();
             Thread.sleep(1000);
             if(tlr.nextInt(0,5)==0) System.out.println(Thread.currentThread().getId()+": Consumed "+l.get(tlr.nextInt(0,10)));
             l.notify();
         }
+		}
     }
     public static void main(String ar[]){
-        pcWaitNotify_8 p = new pcWaitNotify_8();
+       final pcWaitNotify_8 p = new pcWaitNotify_8();
         Thread ct = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    consumer();
+                    p.consumer();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -42,7 +45,7 @@ public class pcWaitNotify_8 {
             @Override
             public void run() {
                 try {
-                    producer();
+                    p.producer();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
