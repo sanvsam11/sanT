5152a75c272c4ade4ac7d590061ebc0f1bfb7a0f
Santhosh Varathan <SanthoshVarathan@MacBook-Air.local>
2017-03-12 19:12:48 +0530

    loan comit

---
+++ LoanApplication/src/LoanApplication.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SanthoshVarathan on 09/03/17.
+ */
+public interface LoanApplication {
+    public boolean applyLoan();
+    public void addAsset();
+    public int applicationStatus(LoanApplication instance);
+    
+}
